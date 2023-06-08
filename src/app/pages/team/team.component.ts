a97e322f4b1f4ed933557711e13fa81f6828b1b2
Amanda Avelino <amavlopes@gmail.com>
2023-06-08 10:04:32 -0300

    feat(component): team page initial structure

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '@app/core/services/auth.service';
+
 @Component({
   selector: 'tt-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent {
+
+  constructor(private auth: AuthService) { }
+
+  logout() {
+    this.auth.logout();
+  }
 
 }
